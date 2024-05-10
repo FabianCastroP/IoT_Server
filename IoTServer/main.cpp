6f9534d9419e6152feec23fcd43842eb2f7cec20
Fabián Luciano Castro Ponce <fabianluciano.castro.ponce@alumnos.upm.es>
2024-05-10 17:11:08 +0200

    Servidor funcionando

--- IoTServer/main.cpp
+++ IoTServer/main.cpp
@@ -3,7 +3,7 @@
 
 int main(void) {
     std::string serverAddress = "192.168.1.168";
-    std::string clientAddress = "192.168.1.168";
+    std::string clientAddress = "192.168.1.228";
     std::string serverPort = "5000";
     std::string clientPort = "5000";
     const char* fileName = "log.txt";
@@ -16,18 +16,15 @@ int main(void) {
         char buffer[1000];	//11
 
         while (true) {
-        	std::cout << "Sending message" << std::endl;
-        	int sent_bytes = server.Send(clientAddress, clientPort, msg, sizeof(msg));
-        	std::cout << "Message sent" << std::endl;
         	std::cout << "Waiting for message..." << std::endl;
 
             int received_bytes = server.Receive(buffer, sizeof(buffer));
             if (received_bytes > 0) {
                 buffer[received_bytes] = '\0';
                 std::cout << "Received message: " << buffer << std::endl;
-                //msg = OK;
+                msg = OK;
 
-                //server.Send(clientAddress, clientPort, msg, sizeof(msg));
+                server.Send(clientAddress, clientPort, msg, sizeof(msg));
             }
         }
     } catch (const std::exception &e) {
